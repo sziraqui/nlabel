fd9aef7d7d205db52381adef0655e47e703a073c
Sarfaraz Ghulam Iraqui <sarfarazghlm@gmail.com>
2018-03-24 19:53:09 +0530

    dummy images and json

--- public/samples/dummy-data.js
+++ public/samples/dummy-data.js
@@ -24,4 +24,27 @@ var data = [{
     "tag": ['category','age', 'gender', 'skin_tone','hair_color']
 }];
 
+var tags = ['filename','dir','category','age', 'gender', 'skin_tone','hair_color'];
+var labels = [ {
+        "filename": "tech-wallpapers-007.jpg",
+        "dir": "/home/sziraqui/Pictures",
+        "category": "person",
+        "age": 20,
+        "gender": "male",
+        "skin_tone": "fair",
+        "hair_color": "black"
+    }, {
+        "filename": "Selection_002.png",
+        "dir": "/home/sziraqui/Pictures",
+        "category": "person",
+        "age": 22,
+        "gender": "female",
+        "skin_tone": "white",
+        "hair_color": "blonde"
+    }
+];
+
+
 exports.setupData = data;
+exports.tags = tags;
+exports.labels = labels;
