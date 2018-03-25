d7ddc02a0ff63e66aa28556f0bd95b6073e048a6
Sarfaraz Ghulam Iraqui <sarfarazghlm@gmail.com>
2018-03-25 13:34:06 +0530

    dummy data

--- public/samples/dummy-data.js
+++ public/samples/dummy-data.js
@@ -1,27 +1,27 @@
 var data = [{
     "filename": "Webiste logo",
     "src": "images/logo_256x256.png",
-    "tag": ['category','age', 'gender', 'skin_tone','hair_color']
+    "tags": ['category','age', 'gender', 'skin_tone','hair_color']
 }, {
     "filename": "Download icon",
     "src": "images/ic_download_csv_64x64.png",
-    "tag": ['category','age', 'gender', 'skin_tone','hair_color']
+    "tags": ['category','age', 'gender', 'skin_tone','hair_color']
 }, {
     "filename": "Download icon",
     "src": "images/ic_download_csv_64x64.png",
-    "tag": ['category','age', 'gender', 'skin_tone','hair_color']
+    "tags": ['category','age', 'gender', 'skin_tone','hair_color']
 }, {
     "filename": "Download icon",
     "src": "images/ic_download_csv_64x64.png",
-    "tag": ['category','age', 'gender', 'skin_tone','hair_color']
+    "tags": ['category','age', 'gender', 'skin_tone','hair_color']
 }, {
     "filename": "Download icon",
     "src": "images/ic_download_csv_64x64.png",
-    "tag": ['category','age', 'gender', 'skin_tone','hair_color']
+    "tags": ['category','age', 'gender', 'skin_tone','hair_color']
 }, {
     "filename": "Download icon",
     "src": "images/ic_download_csv_64x64.png",
-    "tag": ['category','age', 'gender', 'skin_tone','hair_color']
+    "tags": ['category','age', 'gender', 'skin_tone','hair_color']
 }];
 
 var tags = ['filename','dir','category','age', 'gender', 'skin_tone','hair_color'];
