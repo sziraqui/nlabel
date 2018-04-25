19cbe3aba5941dcc01fbfad04922ef493baf649e
Sarfaraz Ghulam Iraqui <sarfarazghlm@gmail.com>
2018-04-25 18:12:27 +0530

    render UI from config data

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,25 @@ var express = require('express');
 var router = express.Router();
 var parseTagsNlabels = require('../tools.js').parseTagsNlabels;
 var getImagesByDir = require('../tools.js').getImagesByDir;
+var config = require('../public/data/config.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'NLabel' });
+  substitute = {
+    title: 'NLabel',
+    dir: config.rootDir,
+    imgWidth: config.imageSize.width,
+    imgHeight: config.imageSize.height,
+    classesList: Object.keys(config.classes).join(',')
+  }
+  res.render('index', substitute);
 });
 
 
 router.post('/', function(req, res, next) {
   // get a list of file names
   var fileDir = String(req.body.directoryPath);
-  var tags = String(req.body.tagTextAreaData);
+  var tags = String(req.body.classesInput);
   var tagsArr = tags.split(',');
   getImagesByDir(fileDir, (imageList) => {
     var data = parseTagsNlabels(fileDir, imageList, tagsArr);
